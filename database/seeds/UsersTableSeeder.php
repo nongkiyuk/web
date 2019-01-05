767af5cbdb9267c330ef49461ffccd521fa3ecd1
Aji Kamaludin <aji19kamaludin@gmail.com>
2019-01-05 11:36:17 +0700

    update seeder user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,5 +13,14 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user = factory(App\Models\User::class, 10)->create();
+        $user = new User([
+            'name' => 'Maful Prayoga',
+            'email' => 'maful@example.com',
+            'username' => 'mafulful',
+            'password' => '123456',
+            'is_active' => '1'
+        ]);
+        $user->save();
+        
     }
 }
